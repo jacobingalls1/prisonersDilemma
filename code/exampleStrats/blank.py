344f824c9b30d4d7680512f54d067c09937ff87b
Owen Ingalls <oingalls@mymail.mines.edu>
2021-05-20 04:14:40 -0600

    Added _weighted_random, _recent_weighted, and _anti_detective

--- code/exampleStrats/blank.py
+++
@@ -1,7 +0,0 @@
-def strategy(history, memory):
-    choice = 0 #this is the default/first turn choice
-    if history.shape[1]== 0:
-        return choice, None
-    #history[0] is the list of your choices, history[1] is the list of the opponent's
-    #now make your move
-    return choice, None
