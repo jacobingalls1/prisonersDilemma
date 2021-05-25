7d40d2b2ec7586fd08d3c7282dea087f6b4aef72
Owen Ingalls <oingalls@mymail.mines.edu>
2021-05-24 19:48:37 -0600

    Added all of l4vr0v's strats for testing

--- PrisonersDilemmaTournament/code/strats/detective.py
+++ PrisonersDilemmaTournament/code/strats/detective.py
@@ -7,9 +7,8 @@ import numpy as np
 # DETECTIVE: First: I analyze you. I start:
 # Cooperate, Cheat, Cooperate, Cooperate.
 # If you cheat back, I'll act like [Tit for Tat].
-# If you never cheat back, I'll act like [alwaysDefect],
+# If you never cheta back, I'll act like [alwaysDefect],
 # to exploit you. Elementary, my dear Watson.
-
 
 def strategy(history, memory):
     testingSchedule = [1,0,1,1]
